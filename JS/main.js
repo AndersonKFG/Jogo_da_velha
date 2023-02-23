a36e4ca46d1df0a3cf7572490c87d6766df77f19
Anderson Kauan <anderson.kfgoveia@gmail.com>
2023-02-23 01:05:46 -0300

    Versão 3 - Funcionando!

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,22 @@
-const botao1 = document.getElementById("1")
-const botao2 = document.getElementById("2")
-const botao3 = document.getElementById("3")
-const botao4 = document.getElementById("4")
-const botao5 = document.getElementById("5")
-const botao6 = document.getElementById("6")
-const botao7 = document.getElementById("7")
-const botao8 = document.getElementById("8")
-const botao9 = document.getElementById("9")
-const turno_dit = document.getElementById("turno_dit")
-// botao2.setAttribute("disabled", "")
+const botao1 = document.getElementById("1");
+const botao2 = document.getElementById("2");
+const botao3 = document.getElementById("3");
+const botao4 = document.getElementById("4");
+const botao5 = document.getElementById("5");
+const botao6 = document.getElementById("6");
+const botao7 = document.getElementById("7");
+const botao8 = document.getElementById("8");
+const botao9 = document.getElementById("9");
+const turno_dit = document.getElementById("turno_dit");
 
 var turno = "c"
-console.log("Master TURNO -> " + turno)
 
 function _turno(){
+    
 
     if (turno == "c"){
         turno = "x";
         return turno;
-
     }else if(turno == "x"){
         turno = "c";
         return turno
@@ -30,9 +28,11 @@ botao1.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao1.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao1.setAttribute("disabled", "")
     }
     
 })
@@ -41,9 +41,11 @@ botao2.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao2.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao2.setAttribute("disabled", "")
     }
     
 })
@@ -52,9 +54,11 @@ botao3.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao3.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao3.setAttribute("disabled", "")
     }
     
 })
@@ -63,9 +67,11 @@ botao4.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao4.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao4.setAttribute("disabled", "")
     }
     
 })
@@ -74,9 +80,11 @@ botao5.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao5.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao5.setAttribute("disabled", "")
     }
     
 })
@@ -85,9 +93,11 @@ botao6.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao6.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao6.setAttribute("disabled", "")
     }
     
 })
@@ -96,9 +106,11 @@ botao7.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao7.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao7.setAttribute("disabled", "")
     }
     
 })
@@ -107,9 +119,11 @@ botao8.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao8.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao8.setAttribute("disabled", "")
     }
     
 })
@@ -118,9 +132,11 @@ botao9.addEventListener('click', function(){
     let turno_ = _turno();
 
     if (turno_ == "x"){
-        console.log("X")
+        this.textContent = "X"
+        botao9.setAttribute("disabled", "")
     }else if(turno_ == "c"){
-        console.log("C")
+        this.textContent = "C"
+        botao9.setAttribute("disabled", "")
     }
     
 })
